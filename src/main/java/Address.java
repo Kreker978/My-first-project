6dcf733c441e3aa95005efd56ffde012260007a4
radetskii_sergei <kreker978@yandex.ru>
2022-07-31 01:29:23 +0300

    'initial'

---
+++ src/main/java/Address.java
@@ -0,0 +1,48 @@
+public class Address {
+    private String city;
+    private String street;
+    private int houseNumber;
+    private int houseFloor;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public int getHouseFloor() {
+        return houseFloor;
+    }
+
+    public void setHouseFloor(int houseFloor) {
+        this.houseFloor = houseFloor;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", houseNumber=" + houseNumber +
+                ", houseFloor=" + houseFloor +
+                '}';
+    }
+}
